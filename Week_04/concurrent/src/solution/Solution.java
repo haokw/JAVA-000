1b030719e3d7709ebac0efec0cbb587c97c47209
haokw <atwanghao@gmail.com>
2020-11-10 15:20:42 +0800

    作业阶段提交

---
+++ Week_04/concurrent/src/solution/Solution.java
@@ -0,0 +1,12 @@
+package solution;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello Solution");
+    }
+
+    public static String run() {
+        return "Run Solution";
+    }
+}
---
+++ Week_04/concurrent/src/solution/Solution01.java
@@ -0,0 +1,74 @@
+package solution;
+
+
+public class Solution01 {
+    public static void main(String[] args) throws Exception {
+        goSolution1();
+        goSolution2();
+
+        System.out.println("main return");
+    }
+
+    public static void goSolution1() throws Exception {
+        long start = System.currentTimeMillis();
+
+        MyRunnable r = new MyRunnable();
+        Thread t = new Thread(r);
+        t.start();
+        t.join();
+        System.out.printf("result : %s\n", r.getResult());
+        System.out.println("goSolution1 return");
+        // System.out.printf("%s return\n", Thread.currentThread().getStackTrace()[2].getMethodName());
+        // System.out.printf("%s return\n", (new Exception()).getStackTrace()[1].getMethodName());
+
+        long end = System.currentTimeMillis();
+        System.out.printf("time consuming : %s ms\n", (end - start));
+    }
+
+    public static void goSolution2() throws Exception {
+        long start = System.currentTimeMillis();
+
+        MyMethod m = new MyMethod();
+        Thread t = new Thread(()->{
+            m.run();
+        });
+        t.start();
+        t.join();
+        System.out.printf("result : %s\n", m.getResult());
+        System.out.println("goSolution2 return");
+
+        long end = System.currentTimeMillis();
+        System.out.printf("time consuming : %s ms\n", (end - start));
+    }
+
+    public static void goSolution3() throws Exception {
+
+    }
+}
+
+class MyMethod {
+    private String result;
+
+    public void run() {
+        result = "run thread";
+        System.out.println("thread return");
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
+
+class MyRunnable implements Runnable {
+    private String result;
+
+    @Override
+    public void run() {
+        result = "run runnable";
+        System.out.println("runnable return");
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
