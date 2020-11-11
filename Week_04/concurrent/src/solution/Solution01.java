f83ef0671518cb669afb7a75fa9f89216d49fbea
haokw <atwanghao@gmail.com>
2020-11-11 20:41:48 +0800

    week 04 更新

--- Week_04/concurrent/src/solution/Solution01.java
+++ Week_04/concurrent/src/solution/Solution01.java
@@ -1,10 +1,13 @@
 package solution;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 
 public class Solution01 {
     public static void main(String[] args) throws Exception {
         goSolution1();
         goSolution2();
+        goSolution3();
 
         System.out.println("main return");
     }
@@ -42,16 +45,30 @@ public class Solution01 {
     }
 
     public static void goSolution3() throws Exception {
+        long start = System.currentTimeMillis();
 
+        AtomicReference a = new AtomicReference();
+        MyMethod m = new MyMethod();
+        Thread t = new Thread(()->{
+            a.set(m.run());
+        });
+        t.start();
+        t.join();
+        System.out.printf("result : %s\n", a.get());
+        System.out.println("goSolution3 return");
+
+        long end = System.currentTimeMillis();
+        System.out.printf("time consuming : %s ms\n", (end - start));
     }
 }
 
 class MyMethod {
     private String result;
 
-    public void run() {
+    public String run() {
         result = "run thread";
         System.out.println("thread return");
+        return result;
     }
 
     public String getResult() {
