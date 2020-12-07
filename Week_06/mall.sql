49034a77c477752ec83765b58868cad8a78ba4ca
haokw <atwanghao@gmail.com>
2020-12-08 01:48:42 +0800

    update 关系型数据库设计规范

--- Week_06/mall.sql
+++ Week_06/mall.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 8.0.16, for osx10.12 (x86_64)
 --
--- Host: 127.0.0.1    Database: dev
+-- Host: 127.0.0.1    Database: local
 -- ------------------------------------------------------
--- Server version	8.0.18
+-- Server version	8.0.22
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,20 +23,20 @@ DROP TABLE IF EXISTS `tbl_commodity`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `tbl_commodity` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
-  `nickname` varchar(32) DEFAULT NULL COMMENT '商品昵称',
-  `name` varchar(32) NOT NULL COMMENT '商品名称',
-  `description` varchar(255) DEFAULT NULL COMMENT '商品描述',
-  `store_id` bigint(20) DEFAULT NULL COMMENT '店铺ID',
-  `status` smallint(6) DEFAULT NULL COMMENT '商品上架状态',
-  `price` int(11) DEFAULT NULL COMMENT '价格',
-  `stock` int(11) DEFAULT NULL COMMENT '库存',
-  `crime` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  `utime` timestamp NULL DEFAULT NULL COMMENT '更新时间',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+  `nickname` varchar(32) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '商品昵称',
+  `name` varchar(32) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '商品名称',
+  `description` varchar(128) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '商品描述',
+  `store_id` bigint NOT NULL DEFAULT '0' COMMENT '店铺ID',
+  `status` smallint NOT NULL DEFAULT '0' COMMENT '商品上架状态',
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '价格',
+  `stock` int NOT NULL DEFAULT '0' COMMENT '库存',
+  `crime` bigint NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `utime` bigint NOT NULL DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`id`),
   KEY `idx_store_id` (`store_id`) USING BTREE,
   KEY `idx_status` (`status`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,14 +47,14 @@ DROP TABLE IF EXISTS `tbl_order`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `tbl_order` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单ID',
-  `price` int(11) DEFAULT NULL COMMENT '订单总价',
-  `number` int(11) DEFAULT NULL COMMENT '商品个数',
-  `status` smallint(6) DEFAULT NULL COMMENT '订单状态',
-  `phone` bigint(20) DEFAULT NULL COMMENT '订单电话',
-  `address` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '订单地址',
-  `utime` timestamp NULL DEFAULT NULL COMMENT '更新时间',
-  `crime` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '订单ID',
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '订单总价',
+  `number` int NOT NULL DEFAULT '0' COMMENT '商品个数',
+  `status` smallint NOT NULL DEFAULT '0' COMMENT '订单状态',
+  `phone` char(11) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '订单电话',
+  `address` varchar(128) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '订单地址',
+  `utime` bigint NOT NULL DEFAULT '0' COMMENT '更新时间',
+  `crime` bigint NOT NULL DEFAULT '0' COMMENT '创建时间',
   PRIMARY KEY (`id`),
   KEY `idx_status` (`status`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -68,17 +68,17 @@ DROP TABLE IF EXISTS `tbl_order_detail`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `tbl_order_detail` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `order_id` bigint(20) DEFAULT NULL COMMENT '订单ID',
-  `user_id` bigint(20) DEFAULT NULL COMMENT '用户ID',
-  `commodity_id` bigint(20) DEFAULT NULL COMMENT '商品ID',
-  `store_id` bigint(20) DEFAULT NULL COMMENT '店铺ID',
-  `price` int(11) DEFAULT NULL COMMENT '商品价格',
-  `utime` timestamp NULL DEFAULT NULL COMMENT '更新时间',
-  `crime` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `order_id` bigint NOT NULL DEFAULT '0' COMMENT '订单ID',
+  `user_id` bigint NOT NULL DEFAULT '0' COMMENT '用户ID',
+  `commodity_id` bigint NOT NULL DEFAULT '0' COMMENT '商品ID',
+  `store_id` bigint NOT NULL DEFAULT '0' COMMENT '店铺ID',
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '商品价格',
+  `utime` bigint NOT NULL DEFAULT '0' COMMENT '更新时间',
+  `crime` bigint NOT NULL DEFAULT '0' COMMENT '创建时间',
   PRIMARY KEY (`id`),
   KEY `idx_order_id` (`order_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -89,21 +89,21 @@ DROP TABLE IF EXISTS `tbl_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `tbl_user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-  `nickname` varchar(32) DEFAULT '' COMMENT '用户名字',
-  `loginname` varchar(32) NOT NULL COMMENT '登录名',
-  `password` varchar(128) NOT NULL COMMENT '登录密码密文',
-  `phone` bigint(20) DEFAULT NULL COMMENT '手机号码',
-  `address` varchar(255) DEFAULT NULL COMMENT '地址',
-  `crime` timestamp NULL DEFAULT NULL COMMENT '创建时间',
-  `utime` timestamp NULL DEFAULT NULL COMMENT '更新时间',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `nickname` varchar(32) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '用户名字',
+  `loginname` varchar(32) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '登录名',
+  `password` varchar(128) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '登录密码密文',
+  `phone` char(11) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '手机号码',
+  `address` varchar(128) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '地址',
+  `crime` bigint NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `utime` bigint NOT NULL DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`id`),
   KEY `idx_loginname` (`loginname`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping routines for database 'dev'
+-- Dumping routines for database 'local'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -115,4 +115,4 @@ CREATE TABLE `tbl_user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-25 15:45:10
+-- Dump completed on 2020-12-08  1:44:45
